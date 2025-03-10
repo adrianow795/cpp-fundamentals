9355e065dca9c88e6388c208f91bf5343b75df42
adrianow795 <adrianow795@gmail.com>
2025-03-10 17:03:15 +0100

    NWD

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,8 +1,40 @@
 #pragma once
 
+#include <cstdlib>
+#include <algorithm>
+
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    int result  = 0;
+
+    if(lhs == 0 && rhs == 0)
+    {
+        result = 0;
+    }
+    else if(lhs == 0)
+    {
+        result = abs(rhs);
+    }
+    else if(rhs == 0)
+    {
+        result = abs(lhs);
+    }
+    else
+    {
+        int a,b,c,flag;
+        a = std::max(abs(lhs),abs(rhs));
+        b = std::min(abs(lhs),abs(rhs));
+        while(a%c != 0 || b%c != 0)
+        {
+            c--;
+            if(c == 0)
+            {
+                break;
+            }
+        }
+        result = c;
+    }
+
+    return result;
 }
 
 int NWW(int lhs, int rhs) {
